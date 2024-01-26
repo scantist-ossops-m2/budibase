6cb7e4286cf437afe8437a5650c0fb49e06cb753
Gerard Burns <gjburns9@hotmail.com>
2024-01-26 13:51:32 +0000

    wip

---
+++ packages/builder/src/components/design/settings/controls/ChartMultifieldSelect.svelte
@@ -0,0 +1,43 @@
+<script>
+  import { Multiselect } from "@budibase/bbui"
+  import {
+    getDatasourceForProvider,
+    getSchemaForDatasource,
+  } from "builderStore/dataBinding"
+  import { currentAsset } from "builderStore"
+  import { createEventDispatcher } from "svelte"
+
+  export let componentInstance = {}
+  export let value = ""
+  export let placeholder
+
+  const dispatch = createEventDispatcher()
+  $: datasource = getDatasourceForProvider($currentAsset, componentInstance)
+  $: schema = getSchemaForDatasource($currentAsset, datasource).schema
+  $: options = Object.keys(schema || {}).filter(key => {
+    return (
+      schema[key].type !== "json" &&
+      schema[key].type !== "array" &&
+      schema[key].type !== "attachment" &&
+      schema[key].type !== "barcodeqr" &&
+      schema[key].type !== "link" &&
+      schema[key].type !== "bb_reference"
+    );
+  });
+  $: boundValue = getValidOptions(value, options)
+
+  const getValidOptions = (selectedOptions, allOptions) => {
+    // Fix the hardcoded default string value
+    if (!Array.isArray(selectedOptions)) {
+      selectedOptions = []
+    }
+    return selectedOptions.filter(val => allOptions.indexOf(val) !== -1)
+  }
+
+  const setValue = value => {
+    boundValue = getValidOptions(value.detail, options)
+    dispatch("change", boundValue)
+  }
+</script>
+
+<Multiselect {placeholder} value={boundValue} on:change={setValue} {options} />
--- packages/client/src/components/app/charts/ApexChart.svelte
+++ packages/client/src/components/app/charts/ApexChart.svelte
@@ -1,25 +1,93 @@
 <script>
   import { getContext } from "svelte"
-  import { chart } from "svelte-apexcharts"
-  import Placeholder from "../Placeholder.svelte"
+  import ApexCharts from 'apexcharts'
+  import { Icon } from "@budibase/bbui"
 
   const { styleable, builderStore } = getContext("sdk")
   const component = getContext("component")
 
   export let options
+
+  let chartElement;
+  let chart;
+
+  const updateChart = async (newOptions) => {
+    try {
+      await chart?.updateOptions(newOptions)
+    } catch(e) {
+      //console.log(e)
+    }
+  }
+
+  const renderChart = async (newChartElement) => {
+    try {
+      await chart?.destroy()
+      chart = new ApexCharts(newChartElement, options)
+      await chart.render()
+    } catch(e) {
+      //console.log(e)
+    }
+  }
+
+  const isDataValid = (series) => {
+    console.log(series);
+    if (series.length === 0) return true;
+
+    // array of objects form of series
+    if (Array.isArray(series[0]?.data)) {
+      for (let entry of series) {
+        if (entry?.data?.some(datum => {
+          if (datum?.x) {
+            return true
+          }
+
+          else if (Array.isArray(datum)) {
+            return datum.some(datumValue => typeof datumValue === "number");
+          }
+
+          return typeof datum === "number"
+        })) {
+          return true;
+        }
+      }
+
+      return false;
+    // array of numbers form of series
+    } else {
+      return series.some(s => typeof s === "number");
+    }
+  }
+
+  $: renderChart(chartElement)
+  $: updateChart(options)
+  $: dataValid = isDataValid(options?.series || [])
+
+  $: noData = options == null || options?.series?.length === 0
+
+  $: hide = noData || !dataValid
+
 </script>
 
-{#if options}
-  {#key options.customColor}
-    <div use:chart={options} use:styleable={$component.styles} />
-  {/key}
-{:else if $builderStore.inBuilder}
-  <div use:styleable={$component.styles}>
-    <Placeholder />
-  </div>
-{/if}
+{#key options?.customColor}
+  <div class:hide use:styleable={$component.styles} bind:this={chartElement} />
+  {#if $builderStore.inBuilder && noData }
+    <div class="component-placeholder" use:styleable={{ ...$component.styles, normal: {}, custom: null, empty: true }}>
+      <Icon name="Alert" color="var(--spectrum-global-color-static-red-600)" />
+      Add rows to your data source to start using your component
+    </div>
+  {:else if $builderStore.inBuilder && !dataValid}
+    <div class="component-placeholder" use:styleable={{ ...$component.styles, normal: {}, custom: null, empty: true }}>
+      <Icon name="Alert" color="var(--spectrum-global-color-static-red-600)" />
+      Your selected data cannot be displayed in this chart
+    </div>
+  {/if}
+
+{/key}
 
 <style>
+  .hide {
+    display: none;
+  }
   div :global(.apexcharts-legend-series) {
     display: flex !important;
     text-transform: capitalize;
@@ -58,5 +126,26 @@
       .apexcharts-theme-dark .apexcharts-tooltip-series-group.apexcharts-active
     ) {
     padding-bottom: 0;
+  }
+
+  .component-placeholder {
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-start;
+    align-items: center;
+    color: var(--spectrum-global-color-gray-600);
+    font-size: var(--font-size-s);
+    padding: var(--spacing-xs);
+    gap: var(--spacing-s);
+  }
+
+  /* Common styles for all error states to use */
+  .component-placeholder :global(mark) {
+    background-color: var(--spectrum-global-color-gray-400);
+    padding: 0 4px;
+    border-radius: 2px;
+  }
+  .component-placeholder :global(.spectrum-Link) {
+    cursor: pointer;
   }
 </style>
